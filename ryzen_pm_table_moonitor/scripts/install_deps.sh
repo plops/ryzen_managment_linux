1f029ab8c7d1579734e41a3145113880a9783604
wol <wolpumba@gmail.com>
2025-08-23 18:18:54 +0000

    more

--- ryzen_pm_table_moonitor/scripts/install_deps.sh
+++ ryzen_pm_table_moonitor/scripts/install_deps.sh
@@ -1,19 +1,29 @@
+#!/bin/bash
+
+# This script installs dependencies in a folder. I prefer this to fetch_content because it builds faster and needs less storage
+
 export DEPS=~/deps
 
 cd ~/src
 # https://github.com/Dav1dde/glad
 
-git clone https://github.com/glfw/glfw
-git clone https://github.com/ocornut/imgui
-git clone https://github.com/epezent/implot
-git clone https://github.com/taskflow/taskflow
+# git clone https://github.com/glfw/glfw
+# git clone https://github.com/ocornut/imgui
+# git clone https://github.com/epezent/implot
+# git clone https://github.com/taskflow/taskflow
 
-for i in glfw imgui implot taskflow
+export CXXFLAGS="-O3 -march=native -floop-parallelize-all -floop-nest-optimize -floop-interchange"
+export CFLAGS=$CXXFLAGS
+#glfw imgui implot
+
+# glfw is also installed system wide
+# imgui and implot don't come with cmake install instructions
+for i in glfw taskflow
 do
     cd ~/src/$i
     mkdir build_release
     cd build_release
-    cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PATH=$DEPS
+    cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DEPS
     ninja 2>&1 | tee build.log
     ninja install 2>&1 | tee install.log
 done
