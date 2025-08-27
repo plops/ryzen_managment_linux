adb5994576cea2111b01bb41d2e80e67b7e5cc37
wol <wolpumba@gmail.com>
2025-08-27 18:16:06 +0000

    nicer cmake

--- reader/cmake/flags.cmake
+++ reader/cmake/flags.cmake
@@ -2,20 +2,37 @@
 # -march=native -O3 -floop-parallelize-all -floop-nest-optimize -floop-interchange
 # and enable -flto for release
 
+add_compile_options(-Wall -Wextra -pedantic)
 if (CMAKE_BUILD_TYPE STREQUAL "Release")
     message(STATUS "Release build")
-    set(CMAKE_CXX_FLAGS_RELEASE
-            "-O3 -march=native -mtune=native -ffast-math -floop-parallelize-all -floop-nest-optimize -floop-interchange -DNDEBUG"
+    add_compile_options(
+            -O3
+            -march=native
+            -mtune=native
+            -ffast-math
+            -floop-parallelize-all
+            -floop-nest-optimize
+            -floop-interchange
     )
+    add_compile_definitions(NDEBUG)
     set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
 elseif (CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
     message(STATUS "RelWithDebInfo build")
-    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO
-            "-O2 -g -march=native --floop-parallelize-all -floop-nest-optimize -floop-interchange"
+    add_compile_options(
+            -O2
+            -g
+            -march=native
+            -floop-parallelize-all
+            -floop-nest-optimize
+            -floop-interchange
     )
 else ()
     message(STATUS "Debug build")
-    set(CMAKE_CXX_FLAGS_DEBUG
-            "-O0 -g"
+    add_compile_options(
+            -O0
+            -g
+    )
+    add_compile_definitions(
+            DEBUG
     )
 endif ()
