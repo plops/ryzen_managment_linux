229789a6484caf2358d6c785d3d6fba967401c15
wol <wolpumba@gmail.com>
2025-08-29 19:46:42 +0000

    try profile guided optimization (doesn't help)

--- reader/compile.sh
+++ reader/compile.sh
@@ -1,6 +1,16 @@
-g++ -O3 -march=native -mtune=native -ffast-math \
+export CXXFLAGS="
+-O3 -march=native -mtune=native -ffast-math \
     -floop-parallelize-all -floop-nest-optimize \
-    -floop-interchange -DNDEBUG measure.cpp \
+    -floop-interchange -DNDEBUG \
+     measure.cpp eye_capturer.cpp eye_diagram.cpp pm_table_reader.cpp \
     -std=gnu++26 \
-    -flto \
-    `g++ --help=optimize|cut -d " " -f 3|grep -v "="|grep -- -f|grep -v flive-patching|xargs`
+    -fprofile-update=atomic -pthread \
+    -fprofile-generate=pgo \
+"
+#    -flto
+g++ $CXXFLAGS \
+    -o pm_measure_pgo
+#    `g++ --help=optimize|cut -d " " -f 3|grep -v "="|grep -- -f|grep -v flive-patching|xargs`
+./pm_measure_pgo -c 3
+g++ $CXXFLAGS \
+    -o pm_measure
